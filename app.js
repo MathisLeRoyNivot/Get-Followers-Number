bcf2f9d69edd195d5d712dce893b8013acc5dee5
Mathis Le Roy-Nivot <mathis.lrn061198@gmail.com>
2019-06-01 22:12:53 +0200

    Simple web scraping works

--- app.js
+++ app.js
@@ -1,33 +1,50 @@
+// const wait = require('waait');
+// const http = require("http");
+// const { argv } = require("yargs");
+// const routes = require('./routes/routes');
+
+// const { getHTML, getTwitterFollowers, getInstagramFollowers } = require('./recover');
+
+// // Port argument, if nothing entered, default port is 3000
+// const inputPort = argv._[0];
+// const inputTwitter = argv._[1];
+// const inputInstagram = argv._[2];
+// let port = inputPort && !isNaN(inputPort) && (inputPort > 0 && inputPort % 1 === 0 && inputPort < 65536) ? inputPort : 3000;
+
+// const launchRecovery = async () => {
+//     const igPromise = getHTML(`http://www.instagram.com/${inputInstagram}`);
+//     const twPromise = getHTML(`http://www.twitter.com/${inputTwitter}`);
+//     const [igHTML, twHTML] = await Promise.all([igPromise, twPromise]);
+
+//     const instagramCount = await getInstagramFollowers(igHTML);
+//     const twitterCount = await getTwitterFollowers(twHTML);
+
+//     console.log(`${inputInstagram} got ${instagramCount} IG followers \nand ${inputTwitter} got ${twitterCount} Twitter followers.`);
+// }
+
+// launchRecovery;
+
+// //Launch server on port given by the user
+// http.Server(routes.app);
+// routes.app.listen(port, () => {
+//     console.log(`Server is running on port : ${port}\nYou can routing to the server at the following address :\x1b[31m http://localhost:${port}/`)
+// })
+
+// module.exports = {};
+
 const wait = require('waait');
-const http = require("http");
-const { argv } = require("yargs");
-const routes = require('./routes/routes');
 
-const { getHTML, getTwitterFollowers, getInstagramFollowers } = require('./recover');
+const { getHTML, getTwitterFollowers, getInstagramFollowers } = require ('./recover');
 
-// Port argument, if nothing entered, default port is 3000
-const inputPort = argv._[0];
-const inputTwitter = argv._[1];
-const inputInstagram = argv._[2];
-let port = inputPort && !isNaN(inputPort) && (inputPort > 0 && inputPort % 1 === 0 && inputPort < 65536) ? inputPort : 3000;
-
-const launchRecovery = async () => {
-    const igPromise = getHTML(`http://www.instagram.com/${inputInstagram}`);
-    const twPromise = getHTML(`http://www.twitter.com/${inputTwitter}`);
-    const [igHTML, twHTML] = await Promise.all([igPromise, twPromise]);
-
-    const instagramCount = await getInstagramFollowers(igHTML);
-    const twitterCount = await getTwitterFollowers(twHTML);
-
-    console.log(`${inputInstagram} got ${instagramCount} IG followers \nand ${inputTwitter} got ${twitterCount} Twitter followers.`);
+async function go() {
+    const iPromise = getHTML('https://instagram.com/mathis_lrn');
+    const tPromise = getHTML('https://twitter.com/mnivot');
+    const [instagramHTML, twitterHTML] = await Promise.all([iPromise, tPromise]);
+    const instagramCount = await getInstagramFollowers(instagramHTML);
+    const twCount = await getTwitterFollowers(twitterHTML);
+    console.log(
+        `You have ${twCount} twitter followers and ${instagramCount} instagram followers`
+    );
 }
 
-launchRecovery;
-
-//Launch server on port given by the user
-http.Server(routes.app);
-routes.app.listen(port, () => {
-    console.log(`Server is running on port : ${port}\nYou can routing to the server at the following address :\x1b[31m http://localhost:${port}/`)
-})
-
-module.exports = {};
+go();
--- recover.js
+++ recover.js
@@ -1,12 +1,36 @@
+// const axios = require('axios');
+// const cheerio = require('cheerio');
+
+// const getHTML = async (url) => {
+//     const { data: html } = await axios.get(url)
+//     return html;
+// }
+
+// const getTwitterFollowers = async (html) => {
+//     const $ = cheerio.load(html);
+//     const span = $('[data-nav="followers"] .ProfileNav-value');
+//     return span.data('count');
+// }
+
+// const getInstagramFollowers = async (html) => {
+//     const $ = cheerio.load(html);
+//     const stringData = $('script[type=application/ld+json]').html();
+//     const pageObject = JSON.parse(stringData);
+//     return parseInt(pageObject.mainEntityofPage.interactionStatistic.userInteractionCount);
+// }
+
 const axios = require('axios');
 const cheerio = require('cheerio');
 
 const getHTML = async (url) => {
-    const { data: html } = await axios.get(url)
+    const {
+        data: html
+    } = await axios.get(url);
     return html;
 }
 
 const getTwitterFollowers = async (html) => {
+    // load up cheerio
     const $ = cheerio.load(html);
     const span = $('[data-nav="followers"] .ProfileNav-value');
     return span.data('count');
@@ -14,9 +38,16 @@ const getTwitterFollowers = async (html) => {
 
 const getInstagramFollowers = async (html) => {
     const $ = cheerio.load(html);
-    const stringData = $('script[type=application/ld+json]').html();
-    const pageObject = JSON.parse(stringData);
-    return parseInt(pageObject.mainEntityofPage.interactionStatistic.userInteractionCount);
+    const dataInString = $('script[type="application/ld+json"]').html();
+    const pageObject = JSON.parse(dataInString);
+    return parseInt(
+        pageObject.mainEntityofPage.interactionStatistic.userInteractionCount
+    );
+
+    // console.log($.html());
+    // const span = $('[data-nav="followers"] .ProfileNav-value');
+    // return span.data('count');
+    //            <script type="application/ld+json">
 }
 
 module.exports = {
