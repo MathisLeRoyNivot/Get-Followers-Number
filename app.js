cb068df364fe1e7009b740d8c23405f5cc5336ae
Mathis Le Roy-Nivot <mathis.lrn061198@gmail.com>
2019-06-02 22:51:41 +0200

    Scraping launch with arguments to specify the twitter & intagram pseudo

--- app.js
+++ app.js
@@ -32,19 +32,35 @@
 
 // module.exports = {};
 
+const http = require("http");
+const { argv } = require("yargs");
 const wait = require('waait');
+const routes = require('./routes/routes');
 
 const { getHTML, getTwitterFollowers, getInstagramFollowers } = require ('./recover');
 
-async function go() {
-    const iPromise = getHTML('https://instagram.com/mathis_lrn');
-    const tPromise = getHTML('https://twitter.com/mnivot');
+// // Port argument, if nothing entered, default port is 3000
+// const inputPort = argv._[0];
+const inputInstagram = argv._[0];
+const inputTwitter = argv._[1];
+// let port = inputPort && !isNaN(inputPort) && (inputPort > 0 && inputPort % 1 === 0 && inputPort < 65536) ? inputPort : 3000;
+
+const launchRecovery = async () => {
+    const iPromise = getHTML(`https://instagram.com/${inputInstagram}`);
+    const tPromise = getHTML(`https://twitter.com/${inputTwitter}`);
     const [instagramHTML, twitterHTML] = await Promise.all([iPromise, tPromise]);
     const instagramCount = await getInstagramFollowers(instagramHTML);
     const twCount = await getTwitterFollowers(twitterHTML);
-    console.log(
-        `You have ${twCount} twitter followers and ${instagramCount} instagram followers`
-    );
+    console.log(`Twitter account : @${inputTwitter}\nTwitter followers : ${twCount} \n\nInstagram account : @${inputInstagram}\nInstagram followers : ${instagramCount}`);
 }
 
-go();
+launchRecovery.apply();
+
+//Launch server on port given by the user
+// http.Server(routes.app);
+// routes.app.listen(port, () => {
+//     launchRecovery;
+//     console.log(`Server is running on port : ${port}\nYou can routing to the server at the following address :\x1b[31m http://localhost:${port}/`);
+// })
+
+module.exports = {}
